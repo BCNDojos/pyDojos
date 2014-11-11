2399de3a2462f49d7c80670031f1678dccd86686
Ignasi Fosch <natx@y10k.ws>
2014-11-11 01:16:28 +0100

    Adds third test

--- string-calculator/tests.py
+++ string-calculator/tests.py
@@ -7,11 +7,12 @@ class TestKata(unittest.TestCase):
     def setUp(self):
         self.calc = StringCalculator()
 
-    def test_empty(self):
+    def test_no_operands(self):
         self.assertEqual(self.calc.Add(""), 0)
 
-    def test_one(self):
+    def test_one_operand(self):
         self.assertEqual(self.calc.Add("1"), 1)
+        self.assertEqual(self.calc.Add("2"), 2)
 
 if __name__ == '__main__':
     unittest.main()
