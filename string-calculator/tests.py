068ea9c618579881940d39317930aa51ae6812c0
Ignasi Fosch <natx@y10k.ws>
2014-11-11 09:13:24 +0100

    Añade test para más de dos operandos

--- string-calculator/tests.py
+++ string-calculator/tests.py
@@ -10,12 +10,11 @@ class TestKata(unittest.TestCase):
     def test_no_operands(self):
         self.assertEqual(self.calc.Add(""), 0)
 
-    def test_one_operand(self):
+    def test_n_operands(self):
         self.assertEqual(self.calc.Add("1"), 1)
         self.assertEqual(self.calc.Add("2"), 2)
-
-    def test_two_operands(self):
         self.assertEqual(self.calc.Add("1,2"), 3)
+        self.assertEqual(self.calc.Add("1,2,3"), 6)
 
 if __name__ == '__main__':
     unittest.main()
