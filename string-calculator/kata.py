c24670986a5c45ad36bb5d4998d1cf3d30e35e40
Ignasi Fosch <natx@y10k.ws>
2014-11-13 13:20:17 +0100

    Revert "Añade test para la selección de delimitador"
    This reverts commit cf7b777346bb24e14aaca1c328d762d60c3deaeb.

--- string-calculator/kata.py
+++ string-calculator/kata.py
@@ -1,4 +1,3 @@
 class StringCalculator(object):
   def Add(self, operands):
-    delimiter = ''
-    return sum([int("0" + x) for x in operands.replace(delimiter, ",").replace("\n", ",").split(",")])
+    return sum([int("0" + x) for x in operands.replace("\n", ",").split(",")])
