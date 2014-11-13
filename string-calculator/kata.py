cf7b777346bb24e14aaca1c328d762d60c3deaeb
Ignasi Fosch <natx@y10k.ws>
2014-11-13 13:19:55 +0100

    Añade test para la selección de delimitador

--- string-calculator/kata.py
+++ string-calculator/kata.py
@@ -1,3 +1,4 @@
 class StringCalculator(object):
   def Add(self, operands):
-    return sum([int("0" + x) for x in operands.replace("\n", ",").split(",")])
+    delimiter = ''
+    return sum([int("0" + x) for x in operands.replace(delimiter, ",").replace("\n", ",").split(",")])
