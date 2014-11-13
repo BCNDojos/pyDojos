8eb77844daed7dae33f6f86c31828322e8afbb05
Ignasi Fosch <natx@y10k.ws>
2014-11-13 13:06:09 +0100

    Añade el separador \n(newline)

--- string-calculator/kata.py
+++ string-calculator/kata.py
@@ -1,3 +1,3 @@
 class StringCalculator(object):
   def Add(self, operands):
-    return sum([int('0' + x) for x in operands.split(',')])
+    return sum([int("0" + x) for x in operands.replace("\n", ",").split(",")])
