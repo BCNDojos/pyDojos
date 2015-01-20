b026d22a47bceb5a12b432619b8559a0d3dccf74
Ignasi Fosch <natx@y10k.ws>
2015-01-21 00:38:27 +0100

    Segon test: cas cadena d'un caràcter

--- anagrams/tests.py
+++ anagrams/tests.py
@@ -7,6 +7,7 @@ class TestAnagrams(unittest.TestCase):
      def test_basic_anagrams(self):
          '''tests basic anagrams'''
          self.assertEqual([''], anagramize(''))
+         self.assertEqual(['a'], anagramize('a'))
 
 if __name__ == '__main__':
     unittest.main()
