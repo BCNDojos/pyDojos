672af28e4cad6bfa4b47a99592e7d6e622e5a1c9
Ignasi Fosch <natx@y10k.ws>
2015-01-21 13:10:47 +0100

    Resolució quart test

--- anagrams/kata.py
+++ anagrams/kata.py
@@ -1,5 +1,12 @@
 def anagramize(word):
     if len(word) < 2:
       return [word]
-    else:
+    elif len(word) < 3:
       return [word, word[::-1]]
+    else:
+      return [word,
+              word[0] + word[2] + word[1],
+              word[1] + word[0] + word[2],
+              word[1] + word[2] + word[0],
+              word[2] + word[0] + word[1],
+              word[::-1]]
