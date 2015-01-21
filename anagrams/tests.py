07ca29c47efd239675ef289cbc515cdfaec64f9d
Ignasi Fosch <natx@y10k.ws>
2015-01-21 13:26:56 +0100

    Cinqué test: Test de longitud variable

--- anagrams/tests.py
+++ anagrams/tests.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 
+import string
 import unittest
+from math import factorial
+from random import randint, sample
 from kata import *
 
 class TestAnagrams(unittest.TestCase):
@@ -10,13 +13,17 @@ class TestAnagrams(unittest.TestCase):
          self.assertEqual(['a'], anagramize('a'))
          self.assertEqual(['ab', 'ba'], anagramize('ab'))
 
-     def test_3_length_anagram(self):
-         anagrams = anagramize('abc')
-         self.assertEqual(6,len(set(anagrams)))
-         for word in anagrams:
-             self.assertEqual(3, len(word))
-             for char in set('abc'):
-                 self.assertTrue(char in word)
+     def test_variable_length_anagram(self):
+         alphabet = string.ascii_lowercase
+         length = randint(4,10)
+         word = ''.join(sample(alphabet, length))
+         anagrams_length = factorial(length)
+         anagrams = anagramize(word)
+         self.assertEqual(anagrams_length, len(set(anagrams)))
+         for anagram in anagrams:
+             self.assertEqual(length, len(anagram))
+             for char in set(word):
+                 self.assertTrue(char in anagram)
 
 if __name__ == '__main__':
     unittest.main()
