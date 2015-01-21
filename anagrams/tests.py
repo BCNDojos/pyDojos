e516e4db51b51b2098fd484289b1430de46bce54
Ignasi Fosch <natx@y10k.ws>
2015-01-21 12:49:14 +0100

    Quart test: cas cadena de tres caràcters

--- anagrams/tests.py
+++ anagrams/tests.py
@@ -10,5 +10,13 @@ class TestAnagrams(unittest.TestCase):
          self.assertEqual(['a'], anagramize('a'))
          self.assertEqual(['ab', 'ba'], anagramize('ab'))
 
+     def test_3_length_anagram(self):
+         anagrams = anagramize('abc')
+         self.assertEqual(6,len(set(anagrams)))
+         for word in anagrams:
+             self.assertEqual(3, len(word))
+             for char in set('abc'):
+                 self.assertTrue(char in word)
+
 if __name__ == '__main__':
     unittest.main()
