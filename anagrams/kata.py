e2bc9a0fd25fa463c73c7d6723396d4aed24da7a
Gabi de Maeztu <gabi@chromabranding.com>
2015-01-26 01:03:39 +0100

    Codigo de Josep & Gabi

--- anagrams/kata.py
+++ anagrams/kata.py
@@ -1,2 +1,13 @@
 def anagramize(word):
-    pass
+    l = len(word)
+    r = range(l)
+
+    if l <= 1:
+        return [word]
+    else:
+        result = []
+        for i in r:
+            child_list = anagramize(word[:i] + word[i+1:])
+            for j in child_list:
+                result.append(word[i] + j)
+        return result
