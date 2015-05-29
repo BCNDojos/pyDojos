40faa30695cca55fdb59486a076c4439e58296e1
Ignasi Fosch <natx@y10k.ws>
2015-05-29 20:32:06 +0200

    First version of the GoL statement and support file

---
+++ GoL/gol_utils.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+def create_world(alive = None):
+    world = np.zeros((3,3), dtype=int)
+    if alive:
+        for position in alive:
+            world[position[0], position[1]] = 1
+    return world
+
+def draw_world(world):
+    plt.imshow(world, cmap=plt.cm.Greys, interpolation='nearest')
