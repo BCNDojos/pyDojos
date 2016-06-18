5d78de7fe376691a7eb2dfff6ad94319ac2704c5
Ignasi Fosch <natx@y10k.ws>
2016-06-18 14:03:11 +0200

    Weather kata

--- factor-out/test_weather.py
+++
@@ -1,9 +0,0 @@
-import weather
-
-def test_process_weather(capsys):
-    weather.process()
-    out, err = capsys.readouterr()
-    assert out == "9 32\n"
-
-def test_benchmark_process_weather(benchmark):
-    benchmark(weather.process)
