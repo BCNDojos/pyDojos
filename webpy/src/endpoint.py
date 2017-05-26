94527182797c0976c05dd405b052990dcd19f8fc
jaume <jmila@leadtech.com>
2017-05-27 01:56:26 +0200

    webpy dojo: Print a Form

--- webpy/src/endpoint.py
+++ webpy/src/endpoint.py
@@ -1,26 +1,35 @@
 import web
 import os
 
+from web import form
+
 CWD = os.path.basename(os.path.dirname(os.path.realpath(__file__)))
 
 render = web.template.render('{!s}/templates/'.format(CWD))
 
 urls = (
-  '/', 'index'
+  '/', 'Index'
 )
 
 
-class index:
+class Index:
 
     def GET(self):
-        fullname = 'John Doe'
+        fullname = 'people'
         title = 'This template Title'
         items = [
             "First",
             "Second",
             "Third"
         ]
-        return render.index(fullname, title, items)
+        login = form.Form(
+            form.Textbox('username'),
+            form.Password('password'),
+            form.Button('Login'),
+        )
+        my_form = login()
+        form_html = my_form.render()
+        return render.index(fullname, title, items, form_html)
 
 if __name__ == "__main__":
     app = web.application(urls, globals())
