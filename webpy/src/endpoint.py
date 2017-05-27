7baec59a997ed315b9823bb654cf6e57b8bd0b92
jaume <jmila@leadtech.com>
2017-05-27 02:38:21 +0200

    webpy dojo: Submit Form

--- webpy/src/endpoint.py
+++ webpy/src/endpoint.py
@@ -8,8 +8,42 @@ CWD = os.path.basename(os.path.dirname(os.path.realpath(__file__)))
 render = web.template.render('{!s}/templates/'.format(CWD))
 
 urls = (
-  '/', 'Index'
+  '/', 'Index',
+  '/login', 'Login'
 )
+
+
+class Login:
+
+    login_form = form.Form(
+        form.Textbox('username', form.notnull),
+        form.Password(
+            'password',
+            form.notnull,
+            form.regexp('\d+', 'Digits only'),
+            form.Validator(
+                'Must be larger or equal than 8',
+                lambda x: len(x) >= 8
+            )
+        )
+    )
+
+    def GET(self):
+        my_form = self.login_form()
+        return render.login(my_form)
+
+    def POST(self):
+        my_form = self.login_form()
+        if not my_form.validates():
+            return render.login(my_form)
+        else:
+            result = 'my_form.d.username and my_form.["username"].value are ' \
+                     'equivalent ways of extracting the validated arguments ' \
+                     'from the form.\n\n'
+            result += 'Got! username: "{!s}", password: "{!s}"'.format(
+                my_form['username'].value, my_form['password'].value
+            )
+            return result
 
 
 class Index:
@@ -22,14 +56,8 @@ class Index:
             "Second",
             "Third"
         ]
-        login = form.Form(
-            form.Textbox('username'),
-            form.Password('password'),
-            form.Button('Login'),
-        )
-        my_form = login()
-        form_html = my_form.render()
-        return render.index(fullname, title, items, form_html)
+
+        return render.index(fullname, title, items)
 
 if __name__ == "__main__":
     app = web.application(urls, globals())
