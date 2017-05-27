354fae2d8a8c2f69fe1b31f0064e58b5632b29d0
jaume <jmila@leadtech.com>
2017-05-27 03:36:42 +0200

    webpy dojo: Basic Authentication

--- webpy/src/endpoint.py
+++ webpy/src/endpoint.py
@@ -1,3 +1,6 @@
+import base64
+import re
+
 import web
 import os
 
@@ -9,31 +12,58 @@ render = web.template.render('{!s}/templates/'.format(CWD))
 
 urls = (
   '/', 'Index',
-  '/login', 'Login'
+  '/login', 'Login',
+  '/private/', 'Private',
 )
+
+
+class Private:
+
+    def __init__(self):
+        self._allowed = [
+            ('admin', '12345678'),
+            ('test', '87654321')
+        ]
+
+    def GET(self):
+        auth = web.ctx.env.get('HTTP_AUTHORIZATION')
+        if auth is None:
+            auth_is_required = True
+        else:
+            auth = re.sub('^Basic ', '', auth)
+            username, password = base64.decodestring(auth).split(':')
+            if (username, password) in self._allowed:
+                return render.private(username)
+            else:
+                auth_is_required = True
+        if auth_is_required:
+            web.header('WWW-Authenticate', 'Basic realm="Auth example"')
+            web.ctx.status = '401 Unauthorized'
+            return
 
 
 class Login:
 
-    login_form = form.Form(
-        form.Textbox('username', form.notnull),
-        form.Password(
-            'password',
-            form.notnull,
-            form.regexp('\d+', 'Digits only'),
-            form.Validator(
-                'Must be larger or equal than 8',
-                lambda x: len(x) >= 8
+    def __init__(self):
+        self._login_form = form.Form(
+            form.Textbox('username', form.notnull),
+            form.Password(
+                'password',
+                form.notnull,
+                form.regexp('\d+', 'Digits only'),
+                form.Validator(
+                    'Must be larger or equal than 8',
+                    lambda x: len(x) >= 8
+                )
             )
         )
-    )
 
     def GET(self):
-        my_form = self.login_form()
+        my_form = self._login_form()
         return render.login(my_form)
 
     def POST(self):
-        my_form = self.login_form()
+        my_form = self._login_form()
         if not my_form.validates():
             return render.login(my_form)
         else:
