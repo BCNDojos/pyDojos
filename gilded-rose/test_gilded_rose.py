0b31576c0c704e587ddea7b3ac5f4d59ff94967a
Ignasi Fosch <natx@y10k.ws>
2017-10-12 19:11:29 +0200

    Add single normal item test

--- gilded-rose/test_gilded_rose.py
+++ gilded-rose/test_gilded_rose.py
@@ -4,8 +4,17 @@ from gilded_rose import GildedRose
 from item import Item
 
 
-def test_foo():
-    items = [Item("foo", 0, 0)]
+def test_normal_items():
+    # Arrange
+    test_case = (
+        Item("+5 Dexterity Vest", 2, 4),
+        Item("+5 Dexterity Vest", 1, 3),
+    )
+    items = [test_case[0]]
     gilded_rose = GildedRose(items)
+    # Act
     gilded_rose.update_quality()
-    assert "fixme" == items[0].name
+    # Assert
+    for item in items:
+        assert item.sell_in == test_case[1].sell_in
+        assert item.quality == test_case[1].quality
